affc0e0852d09a54494e14e91b41d4e042013fb7
philbjern <filipbiernat@gmail.com>
2025-04-12 14:16:53 +0200

    feat: update api.js

--- routes/api.js
+++ routes/api.js
@@ -1,9 +1,16 @@
 'use strict';
 
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+
 module.exports = function (app) {
   
-  app.route('/api/threads/:board');
+  app.route('/api/threads/:board', (req, res) => {
+    res.json({ message: 'Threads API' });
+  });
     
-  app.route('/api/replies/:board');
+  app.route('/api/replies/:board', (req, res) => {
+    res.json({ message: 'Replies API' });
+  });
 
 };
